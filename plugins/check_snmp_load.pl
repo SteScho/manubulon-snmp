5aa4264881193d16b12fcafad6c5ee5a65a9f54b
Patrick Proy <patrick@proy.org>
2006-06-30 22:54:33 +0000

    - Full snmpv3 support - Timeout bug corrected - Better handling when no information with netsl - Backward compatibility for version <1.2 removed

--- plugins/check_snmp_load.pl
+++ plugins/check_snmp_load.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w 
 ############################## check_snmp_load #################
-# Version : 1.2.2 
-# Date : Jun 27 2006
+# Version : 1.3 
+# Date : Jun 30 2006
 # Author  : Patrick Proy ( patrick at proy.org)
 # Help : http://www.manubulon.com/nagios/
 # Licence : GPL - http://www.fsf.org/licenses/gpl.txt
@@ -88,7 +88,7 @@ my %cpu_oid = ("netsc",$ns_cpu_idle,"as400",$as400_cpu,"bc",$bluecoat_cpu,"nokia
 
 # Globals
 
-my $Version='1.2.2';
+my $Version='1.3';
 
 my $o_host = 	undef; 		# hostname
 my $o_community = undef; 	# community
@@ -98,29 +98,28 @@ my $o_verb=	undef;		# verbose mode
 my $o_version=	undef;		# print version
 # check type  : stand | netsc |  netsl | as400 | cisco | cata | nsc | fg | bc | nokia | hp | lp 
 my $o_check_type= "stand";	
-# For backward compatibility
-my $o_linux=	undef;		# Check linux load instead of CPU
-my $o_linuxC=	undef;		# Check Net-SNMP CPU
-my $o_as400=	undef;		# Check for AS 400 load
-my $o_cisco=	undef;		# Check for Cisco CPU
 # End compatibility
 my $o_warn=	undef;		# warning level
 my @o_warnL=	undef;		# warning levels for Linux Load or Cisco CPU
 my $o_crit=	undef;		# critical level
 my @o_critL=	undef;		# critical level for Linux Load or Cisco CPU
-my $o_timeout=  5;             	# Default 5s Timeout
+my $o_timeout=  undef; 		# Timeout (Default 5)
 my $o_perf=     undef;          # Output performance data
 my $o_version2= undef;          # use snmp v2c
 # SNMPv3 specific
 my $o_login=	undef;		# Login for snmpv3
 my $o_passwd=	undef;		# Pass for snmpv3
+my $v3protocols=undef;	# V3 protocol list.
+my $o_authproto='md5';		# Auth protocol
+my $o_privproto='des';		# Priv protocol
+my $o_privpass= undef;		# priv password
 
 # functions
 
 sub p_version { print "check_snmp_load version : $Version\n"; }
 
 sub print_usage {
-    print "Usage: $0 [-v] -H <host> -C <snmp_community> [-2] | (-l login -x passwd)  [-p <port>] -w <warn level> -c <crit level> -T=[stand|netsl|netsc|as400|cisco|cata|nsc|fg|bc|nokia|hp|lp] [-f] [-t <timeout>] [-V]\n";
+    print "Usage: $0 [-v] -H <host> -C <snmp_community> [-2] | (-l login -x passwd [-X pass -L <authp>,<privp>])  [-p <port>] -w <warn level> -c <crit level> -T=[stand|netsl|netsc|as400|cisco|cata|nsc|fg|bc|nokia|hp|lp] [-f] [-t <timeout>] [-V]\n";
 }
 
 sub isnnum { # Return true if arg is not a number
@@ -131,7 +130,7 @@ sub isnnum { # Return true if arg is not a number
 
 sub help {
    print "\nSNMP Load & CPU Monitor for Nagios version ",$Version,"\n";
-   print "(c)2004-2005 to my cat Ratoune - Author : Patrick Proy\n\n";
+   print "(c)2004-2006 to my cat Ratoune - Author : Patrick Proy\n\n";
    print_usage();
    print <<EOT;
 -v, --verbose
@@ -144,10 +143,14 @@ sub help {
    community name for the host's SNMP agent (implies v1 protocol)
 -2, --v2c
    Use snmp v2c
--l, --login=LOGIN
-   Login for snmpv3 authentication (implies v3 protocol with MD5)
--x, --passwd=PASSWD
-   Password for snmpv3 authentication
+-l, --login=LOGIN ; -x, --passwd=PASSWD
+   Login and auth password for snmpv3 authentication 
+   If no priv password exists, implies AuthNoPriv 
+-X, --privpass=PASSWD
+   Priv password for snmpv3 (AuthPriv protocol)
+-L, --protocols=<authproto>,<privproto>
+   <authproto> : Authentication protocol (md5|sha : default md5)
+   <privproto> : Priv protocole (des|aes : default des) 
 -P, --port=PORT
    SNMP port (Default 161)
 -w, --warn=INTEGER | INT,INT,INT
@@ -178,8 +181,6 @@ sub help {
    timeout for SNMP in seconds (Default: 5)
 -V, --version
    prints version number
--L, --linux, -A, --as400, -I, --cisco, -N, --netsnmp
-   These options are for backward compatibility (version<1.2) 
 EOT
 }
 
@@ -196,6 +197,8 @@ sub check_options {
         'C:s'   => \$o_community,	'community:s'	=> \$o_community,
 	'l:s'	=> \$o_login,		'login:s'	=> \$o_login,
 	'x:s'	=> \$o_passwd,		'passwd:s'	=> \$o_passwd,
+	'X:s'	=> \$o_privpass,		'privpass:s'	=> \$o_privpass,
+	'L:s'	=> \$v3protocols,		'protocols:s'	=> \$v3protocols,   
         't:i'   => \$o_timeout,       	'timeout:i'     => \$o_timeout,
 	'V'	=> \$o_version,		'version'	=> \$o_version,
 
@@ -203,31 +206,34 @@ sub check_options {
         'c:s'   => \$o_crit,            'critical:s'    => \$o_crit,
         'w:s'   => \$o_warn,            'warn:s'        => \$o_warn,
         'f'     => \$o_perf,            'perfparse'     => \$o_perf,
-	'T:s'	=> \$o_check_type,	'type:s'	=> \$o_check_type,
-#  For backward compatibility	
-	'L'	=> \$o_linux,		'linux'		=> \$o_linux,
-	'A'	=> \$o_as400,		'as400'		=> \$o_as400,
-	'I'	=> \$o_cisco,		'cisco'		=> \$o_cisco,
-	'N'	=> \$o_linuxC,		'netsnmp'	=> \$o_linuxC
-    );
-	# For backward compat
-	if (defined($o_linux)) { $o_check_type="netsl" }
-	if (defined($o_linuxC)) { $o_check_type="netsc" }
-	if (defined($o_as400)) { $o_check_type="as400"}
-	if (defined($o_cisco)) { $o_check_type="cisco"}
+	'T:s'	=> \$o_check_type,	'type:s'	=> \$o_check_type
+	);
     # check the -T option
     my $T_option_valid=0; 
-    foreach (@valid_types) { verb("$_ / $o_check_type"); if ($_ eq $o_check_type) {$T_option_valid=1} };
+    foreach (@valid_types) { if ($_ eq $o_check_type) {$T_option_valid=1} };
     if ( $T_option_valid == 0 ) 
        {print "Invalid check type (-T)!\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
     # Basic checks
+	if (defined($o_timeout) && (isnnum($o_timeout) || ($o_timeout < 2) || ($o_timeout > 60))) 
+	  { print "Timeout must be >1 and <60 !\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
+	if (!defined($o_timeout)) {$o_timeout=5;}
     if (defined ($o_help) ) { help(); exit $ERRORS{"UNKNOWN"}};
     if (defined($o_version)) { p_version(); exit $ERRORS{"UNKNOWN"}};
     if ( ! defined($o_host) ) # check host and filter 
 	{ print_usage(); exit $ERRORS{"UNKNOWN"}}
     # check snmp information
     if ( !defined($o_community) && (!defined($o_login) || !defined($o_passwd)) )
-	{ print "Put snmp login info!\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
+	  { print "Put snmp login info!\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
+	if ((defined($o_login) || defined($o_passwd)) && (defined($o_community) || defined($o_version2)) )
+	  { print "Can't mix snmp v1,2c,3 protocols!\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
+	if (defined ($v3protocols)) {
+	  if (!defined($o_login)) { print "Put snmp V3 login info with protocols!\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
+	  my @v3proto=split(/,/,$v3protocols);
+	  if ((defined ($v3proto[0])) && ($v3proto[0] ne "")) {$o_authproto=$v3proto[0];	}	# Auth protocol
+	  if (defined ($v3proto[1])) {$o_privproto=$v3proto[1];	}	# Priv  protocol
+	  if ((defined ($v3proto[1])) && (!defined($o_privpass))) {
+	    print "Put snmp V3 priv login info with priv protocols!\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
+	}
     # Check warnings and critical
     if (!defined($o_warn) || !defined($o_crit))
  	{ print "put warning and critical info!\n"; print_usage(); exit $ERRORS{"UNKNOWN"}}
@@ -267,7 +273,7 @@ if (defined($TIMEOUT)) {
   verb("Alarm at $TIMEOUT + 5");
   alarm($TIMEOUT+5);
 } else {
-  verb("no timeout defined : $o_timeout + 10");
+  verb("no global timeout defined : $o_timeout + 10");
   alarm ($o_timeout+10);
 }
 
@@ -276,27 +282,43 @@ my ($session,$error);
 if ( defined($o_login) && defined($o_passwd)) {
   # SNMPv3 login
   verb("SNMPv3 login");
-  ($session, $error) = Net::SNMP->session(
+    if (!defined ($o_privpass)) {
+  verb("SNMPv3 AuthNoPriv login : $o_login, $o_authproto");
+    ($session, $error) = Net::SNMP->session(
       -hostname   	=> $o_host,
       -version		=> '3',
       -username		=> $o_login,
       -authpassword	=> $o_passwd,
-      -authprotocol	=> 'md5',
-      -privpassword	=> $o_passwd,
+      -authprotocol	=> $o_authproto,
       -timeout          => $o_timeout
-   );
+    );  
+  } else {
+    verb("SNMPv3 AuthPriv login : $o_login, $o_authproto, $o_privproto");
+    ($session, $error) = Net::SNMP->session(
+      -hostname   	=> $o_host,
+      -version		=> '3',
+      -username		=> $o_login,
+      -authpassword	=> $o_passwd,
+      -authprotocol	=> $o_authproto,
+      -privpassword	=> $o_privpass,
+	  -privprotocol => $o_privproto,
+      -timeout          => $o_timeout
+    );
+  }
 } else {
 	if (defined ($o_version2)) {
-  	      # SNMPv2 Login
-  	          ($session, $error) = Net::SNMP->session(
-  	         -hostname  => $o_host,
-  	         -version   => 2,
-  	         -community => $o_community,
-  	         -port      => $o_port,
-  	         -timeout   => $o_timeout
-  	      );
+		# SNMPv2 Login
+		verb("SNMP v2c login");
+		  ($session, $error) = Net::SNMP->session(
+		 -hostname  => $o_host,
+		 -version   => 2,
+		 -community => $o_community,
+		 -port      => $o_port,
+		 -timeout   => $o_timeout
+		);
   	} else {
 	  # SNMPV1 login
+	  verb("SNMP v1 login");
 	  ($session, $error) = Net::SNMP->session(
 		-hostname  => $o_host,
 		-community => $o_community,
@@ -331,6 +353,7 @@ $session->close;
 my @load = undef;
 my @iload = undef;
 my @oid=undef;
+my $exist=0;
 foreach my $key ( keys %$resultat) {
    verb("OID : $key, Desc : $$resultat{$key}");
    if ( $key =~ /$linload_name/ ) { 
@@ -338,7 +361,13 @@ foreach my $key ( keys %$resultat) {
       $iload[0]= pop(@oid) if ($$resultat{$key} eq "Load-1");
       $iload[1]= pop(@oid) if ($$resultat{$key} eq "Load-5");
       $iload[2]= pop(@oid) if ($$resultat{$key} eq "Load-15");
+	  $exist=1
    }
+}
+
+if ($exist == 0) {
+  print "Can't find snmp information on load : UNKNOWN\n";
+  exit $ERRORS{"UNKNOWN"};
 }
 
 for (my $i=0;$i<3;$i++) { $load[$i] = $$resultat{$linload_load . "." . $iload[$i]}};
